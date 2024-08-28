b4c131aee87302fad9d03a3986d71a6ac0089650
kumar <tinytachyon14341@gmail.com>
2024-08-28 23:26:01 +0530

    stemming, lemmatisation added

--- 2_stemming.py
+++ 2_stemming.py
@@ -11,7 +11,14 @@ paragraph = """After listening to all of you, I feel that the number of Lakhpati
             So, you must help explain this to others. Will you do that?"""
 
 sentences = nltk.sent_tokenize(paragraph)#sentence tokenization
-words = nltk.word_tokenize(paragraph)#word tokenization
+
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+stemmer = PorterStemmer()
+for i in range(len(sentences)):
+    words = nltk.word_tokenize(sentences[i])
+    words = [stemmer.stem(word) for word in words if word not in set(stopwords.words('english'))]
+    sentences[i] = ' '.join(words)
 
 print(sentences)
-print(words)
--- 3_lemmatisation.py
+++ 3_lemmatisation.py
@@ -11,7 +11,14 @@ paragraph = """After listening to all of you, I feel that the number of Lakhpati
             So, you must help explain this to others. Will you do that?"""
 
 sentences = nltk.sent_tokenize(paragraph)#sentence tokenization
-words = nltk.word_tokenize(paragraph)#word tokenization
+
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+lemmatizer = WordNetLemmatizer()
+for i in range(len(sentences)):
+    words = nltk.word_tokenize(sentences[i])
+    words = [lemmatizer.lemmatize(word) for word in words if word not in set(stopwords.words('english'))]   
+    sentences[i] = ' '.join(words)
 
 print(sentences)
-print(words)
